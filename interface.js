e7949a70cf2c8e9a2ecd39f4373540689b35abbf
DarkChocLatte <darkchoclatte@gmail.com>
2016-12-08 12:08:44 +0000

    Started building jQuery interface

---
+++ interface.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+var thermostat = new Thermostat
+
+$("#current-temperature").text(thermostat.temperature);
+
+$(document).click(function(){displayTemperature();});
+
+$("#regulation-up").click(function(){thermostat.up();});
+
+$("#regulation-down").click(function(){thermostat.down();});
+
+$("#reset").click(function(){thermostat.reset();});
+
+
+
+
+
+
+
+
+function displayTemperature(){
+  $("#current-temperature").text(thermostat.temperature);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+});
