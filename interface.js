f608875fe059d16a75901f6b7c9d27c505df121e
Pea M. Tyczynska <CrystalPea@github.com>
2016-12-08 12:25:14 +0000

    Activated Power Saving ON/OFF button

--- interface.js
+++ interface.js
@@ -12,11 +12,16 @@ $("#regulation-down").click(function(){thermostat.down();});
 
 $("#reset").click(function(){thermostat.reset();});
 
-
-
-
-
-
+$("#power-saving").click(function(){
+  thermostat.setPowerSaving();
+  var message;
+  if (thermostat.powerSaving) {
+    message = "OFF";
+  } else {
+    message = "ON";
+  }
+  $("#power-saving").text("Turn Power Saving " + message);
+});
 
 
 function displayTemperature(){
